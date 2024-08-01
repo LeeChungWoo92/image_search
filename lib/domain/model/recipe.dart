36a3549cc618f995bcbe10bc221fcf5178ef5d4b
lcw <skcjddn@gmail.com>
2024-08-01 13:32:53 +0900

    feat: freezed 사용하여 recipe 모델 생성

---
+++ lib/domain/model/recipe.dart
@@ -0,0 +1,16 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'recipe.freezed.dart';
+
+part 'recipe.g.dart';
+
+@freezed
+class Recipe with _$Recipe {
+  const factory Recipe({
+    required String imageUrl,
+    required String user,
+    required String tags,
+  }) = _Recipe;
+  
+  factory Recipe.fromJson(Map<String, Object?> json) => _$RecipeFromJson(json); 
+}
